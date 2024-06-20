e5843a218b0fe0484fecc5111bff561020a0fa0c
Christopher Jagusch <cjjagusc@mtu.edu>
2024-06-19 22:35:01 -0400

    updated map to svg

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     var map = L.map('map', {
         crs: L.CRS.Simple,
         minZoom: -2,
-        maxZoom: 2
+        maxZoom: 3
     });
     
     var bounds = [[0,0], [3456,3456]];
-    var image = L.imageOverlay('ssefmap.png', bounds).addTo(map);
+    var image = L.imageOverlay('ssefmap.svg', bounds).addTo(map);
     map.fitBounds(bounds);
 });
